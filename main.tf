ca7ea967061623ffcf98296c6615a64ae3f2079f
T0206721 - BOURGEOIS Thibault <thibault.bourgeois@thalesgroup.com>
2023-01-17 23:00:56 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,75 @@
+provider "vsphere" {
+  vsphere_server = var.vsphere_server
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = var.datacenter
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.cluster
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.network_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "ubuntu" {
+  name          = "${var.ubuntu_name}"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "learn" {
+  name             = "learn-terraform"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = 2
+  memory   = 1024
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  wait_for_guest_net_timeout = -1
+  wait_for_guest_ip_timeout  = -1
+
+  disk {
+    label            = "disk0"
+    thin_provisioned = true
+    size             = 50
+  }
+
+  guest_id = "ubuntu64Guest"
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.ubuntu.id
+    customize {
+      linux_options {
+        host_name = "hello-world"
+        domain    = "example.com"
+      }
+      network_interface {
+        ipv4_address = "10.100.0.221"
+        ipv4_netmask = 24
+      }
+      ipv4_gateway = "10.100.0.1"
+    }
+  }
+}
+
+output "vm_ip" {
+  value = vsphere_virtual_machine.learn.guest_ip_addresses
+}
